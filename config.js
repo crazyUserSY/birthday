80a5078f48bf710667d4287dfa9b0e552aace5bd
crazyUserSY <139031316+crazyUserSY@users.noreply.github.com>
2023-07-16 11:06:13 +0800

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ var config = {
     // 每句话尽量不要超过15个字,不然展示效果可能不太好
     texts: [
         "送给我",      //这里,每句话结尾的最后一个逗号必须是英文的哦!! 很重要哦!!
-        "心爱的小可爱",  // 同上...
+        "心爱的舒薇狗~",  // 同上...
     ],
     /**
      * imgs 可以不填, 但是如果要填写的话必须遵循下面的格式
@@ -22,12 +22,12 @@ var config = {
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
     desc: {
         turn_on: "开始",
-        play: "音乐",
-        bannar_coming: "颜色",
+        play: "music~",
+        bannar_coming: "color~",
         balloons_flying: "好像少点东西",
         cake_fadein: "蛋糕？",
         light_candle: "蜡烛？",
-        wish_message: "生日快乐",
+        wish_message: "Happy Birthday~",
         story: "A MESSAGE FOR YOU",
     }
 };
